61a5f3a739483bf9bd1b3b78cb4c54877774e4dd
lucasheck <lucasheck23@gmail.com>
2023-05-01 01:00:17 -0400

    1 - Installing TypeScript 2 - Converting searchBox component to TSX

---
+++ src/components/searchBox/searchBox.tsx
@@ -0,0 +1,26 @@
+import { ChangeEventHandler } from "react";
+
+import "./searchBox.css";
+
+type SearchBoxProps = {
+	className: string;
+	placeholder?: string;
+	onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+};
+
+const SearchBox = ({
+	className,
+	placeholder,
+	onChangeHandler,
+}: SearchBoxProps) => {
+	return (
+		<input
+			className={`search-box ${className}`}
+			type="search"
+			placeholder={placeholder}
+			onChange={onChangeHandler}
+		/>
+	);
+};
+
+export default SearchBox;
